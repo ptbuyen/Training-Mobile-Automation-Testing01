4fde793a8956ff59a966b5be0151cab6c545ad7f
Uyen Pham <uyenpham@Uyens-iMac.local>
2022-07-12 12:08:10 +0700

    day01 | launch app

---
+++ src/test/java/Day01/LaunchApp.java
@@ -0,0 +1,43 @@
+package Day01;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class LaunchApp {
+
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> appiumDriver = null;
+        Exception exception = null;
+
+        try {
+            // Desired Capabilities
+            DesiredCapabilities desiredCaps = new DesiredCapabilities();
+            desiredCaps.setCapability("platformName", "Android");
+            desiredCaps.setCapability("automationName", "uiautomator2");
+//            desiredCaps.setCapability("udid", "emulator-5554");
+            desiredCaps.setCapability("appPackage", "com.hahalolo.android.halome");
+            desiredCaps.setCapability("appActivity", "com.halo.presentation.startapp.start.StartAct");
+            desiredCaps.setCapability("udid", "8UEDU17C08002008");
+//            desiredCaps.setCapability("appPackage", "com.wdiodemoapp");
+//            desiredCaps.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
+
+            // Init appium session
+            URL appiumServer = new URL("http://localhost:4723/wd/hub");
+            appiumDriver = new AndroidDriver<MobileElement>(appiumServer, desiredCaps);
+
+            // DEBUG PURPOSE ONLY
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            exception = e;
+        }
+
+        if (appiumDriver == null) {
+            throw new RuntimeException(exception.getMessage());
+        }
+
+    }
+}
